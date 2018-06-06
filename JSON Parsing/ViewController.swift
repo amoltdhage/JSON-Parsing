f376fd7de993a1df9d314c50479eed9d3cb086a8
Amol Dhage <amoltdhage@amols-macbook-pro.local>
2018-06-07 00:49:19 +0530

    Initial Commit

---
+++ JSON Parsing/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JSON Parsing
+//
+//  Created by Amol Dhage on 07/06/18.
+//  Copyright © 2018 AmolTDhage. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
